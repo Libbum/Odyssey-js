234f20799784f56a777b10ca2016a159063edd1a
Tim DuBois <timothy.dubois@chalmers.se>
2015-11-22 14:34:01 +0100

    json manifest process written

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import argparse
+import json
 import os
 import sys
 from pathlib import *
@@ -85,6 +86,7 @@ if __name__ == "__main__":
     2. Create a manifest of images for inclusion into Chromatic.
        For now this is a json format file with big, small and ratio attributes.
 
+    TODO: Manifest needs to be appended, not overwritten.
     TODO: Include smart processing of exif, for obtaining GPS info, dates etc to build smart categories.
           Exif is currently pulled, GPS routines are written, this just needs to be added to the manifest really.
     """
@@ -93,7 +95,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     gallery = Path(Path.home(), Path('web/Odyssey/gallery'))
-
+    manifest = []
     exts = '.jpg', '.png'
     maxsize = (500, 500)
 
@@ -110,9 +112,14 @@ if __name__ == "__main__":
                 im.save(str(thumb))
             except IOError:
                 print("Small image for", infile.relative_to(gallery), "failed.")
+            manifest.append({'small': str(infile.relative_to(gallery)), 'big': str(thumb.relative_to(gallery)), 'aspect_ratio': ratio})
             if args.loud:
                 print("Processing", infile.relative_to(gallery))
                 print("width: %d - height: %d" % im.size) # returns (width, height) tuple
                 print("Ratio: ", ratio)
                 if infile.suffix.lower() == '.jpg':
                     print(get_lat_lon(exif_data))
+    if args.loud:
+        print(json.dumps(manifest, sort_keys=True, indent=4)) #Pretty
+    with open(str(gallery.with_name('manifest.json')),'w') as outFile:
+        outFile.write(json.dumps(manifest, separators=(',', ':'))) #Compact
