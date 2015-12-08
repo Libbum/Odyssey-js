e91c16b22cd1300d7750dd6bb47de935b8a19ca1
Tim DuBois <timothy.dubois@chalmers.se>
2015-12-08 13:29:10 +0100

    More cities, added localname property

--- _require/buildworld.sh
+++ _require/buildworld.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-topojson -o ../assets/data/world.json --id-property SU_A3 --properties name --properties country -- countries.json cities.json
+topojson -o ../assets/data/world.json --id-property SU_A3 --properties name,localname,country -- countries.json cities.json
