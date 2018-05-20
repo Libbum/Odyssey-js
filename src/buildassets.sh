cb57099f741ccfd47f77abcc6917369066806b18
Tim DuBois <tim@neophilus.net>
2018-05-20 11:37:16 +0200

    Streamline asset building

--- src/buildassets.sh
+++ src/buildassets.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+echo "Building World"
 node cityToCoords.js
 topojson -o ../dist/assets/data/world.json  --id-property su_a3 --properties name,localname,country -- countries.json cities.json trips.json
+echo "Building Menu"
+node buildMenu.js
